fcbed62ab4615b9bbb11d65a625e09f9934bc3e5
Peter Teuben <teuben@gmail.com>
2023-02-18 09:41:05 -0500

    add feb 18

--- mk_runs.py
+++ mk_runs.py
@@ -21,6 +21,9 @@ project="2023-S1-MX-55"
 
 #        obsnums per source (make it negative if not added to the final combination)
 on = {}
+on["3CR123.0"] = [ 106007, 106008, 106009, 106011, 106012, 106013, 106015, 106016,
+                   106017, 106019, 106020, 106021,]                                 # feb 18
+
 on["3CR234.0"] = [ 105304, 105305, 105306, 105308, 105309, 105310, 105312, 105313,
                    105314, 105316, 105317, 105318,                                  # 10-feb
                    105408, 105409, 105410,]                                         # 14-feb
@@ -32,6 +35,7 @@ on["3CR287.1"] = [ 105324, 105325, 105326, 105328, 105329, 105330, 105332, 10533
 
 #        common parameters per source on the first dryrun (run1a, run2a)
 pars1 = {}
+pars1["3CR123.0"] = ""   # strong continuum
 pars1["3CR234.0"] = ""
 pars1["3CR287.1"] = ""
 
